2776aff0e389488474832582d764a1daf7b5c2a6
ikaroamorim <ikaro.amorim@gmail.com>
2021-08-15 15:16:12 -0300

    Completed Filter Logic

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './styles.module.scss'
+import logoPromobit from '../../../assets/logo_promobit.png'
+
+export function Header() {
+
+   return (
+      <header className={styles.main}>
+         <Link href="/">
+            <div className={styles.logoContainer}>
+               <Image
+                  height={50}
+                  objectFit={'scale-down'}
+                  src={logoPromobit}
+                  alt="Site Logo"
+               >
+               </Image>
+            </div>
+         </Link>
+      </header>
+   )
+
+}
--- src/types/IMovie.ts
+++ src/types/IMovie.ts
@@ -2,6 +2,7 @@ export interface IMovie {
     adult: boolean;
     backdrop_path: string;
     genre_ids: number[];
+    rank: number;
     original_language: string;
     original_title: string;
     overview: string;
