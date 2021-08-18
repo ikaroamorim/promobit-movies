e3574b0d97b66f9d5d0e3cbea7b94d2f3ea8918b
ikaroamorim <ikaro.amorim@gmail.com>
2021-08-17 21:09:42 -0300

    SEO improvements

--- src/pages/movies/[slug].tsx
+++ src/pages/movies/[slug].tsx
@@ -2,6 +2,7 @@ import { GetStaticProps, GetStaticPaths } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Params } from 'next/dist/server/router';
+import Head from 'next/head';
 
 import { api } from '../../services/api';
 
@@ -16,6 +17,16 @@ export default function Movies(props: ISlugProps) {
 
    return (
       <div className={styles.container}>
+         <Head>
+            <title> {props.details.title} - Promobit - Movies</title>
+            <meta name="description" content="Portal da Promobit onde pode-se consultar os filmes mais populares do dia" />
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png"></link>
+            <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png"></link>
+            <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png"></link>
+            <link rel="icon" type="image/png" sizes="192x192" href="/assets/favicon-192x192.png"></link>
+            <link rel="icon" type="image/png" sizes="194x194" href="/assets/favicon-194x194.png"></link>
+         </Head>
          <div className={styles.imgContainer}>
             <Image
                src={`https://image.tmdb.org/t/p/w500${props.details.backdrop_path}`}
@@ -28,9 +39,8 @@ export default function Movies(props: ISlugProps) {
             >
                <a><RiArrowGoBackFill /></a>
             </Link>
-            <h3>Título do filme</h3>
-            <p> {props.details.title}</p>
-            <p> {props.details.original_title} / ({props.details.original_language})</p>
+            <h1> {props.details.title}</h1>
+            <h2> {props.details.original_title} / ({props.details.original_language})</h2>
             {props.details.tagline ? (
                <>
                   <h3>Slogan do Filme</h3>
